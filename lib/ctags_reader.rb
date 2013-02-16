c467218eb2940943d685f155a974676e5220b668
Andrew Radev <andrey.radev@gmail.com>
2013-02-16 17:21:49 +0100

    Shorthand to generate tags

--- lib/ctags_reader.rb
+++ lib/ctags_reader.rb
@@ -1,1 +1,11 @@
 require 'ctags_reader/reader'
+
+module CtagsReader
+  # While the reader attempts to parse different formats of ctags, the
+  # .generate method generates ruby tags in the most convenient way for the
+  # reader.
+  #
+  def self.generate
+    system 'ctags --languages=ruby --excmd=number -R .'
+  end
+end
